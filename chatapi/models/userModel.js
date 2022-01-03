494e13dea30cf3af90e802e6e1391e679627ceea
Nischal Khatri <nischal06khatri@gmail.com>
2022-01-03 14:50:30 +0545

    changes in api and ignored web app

--- chatapi/models/userModel.js
+++ chatapi/models/userModel.js
@@ -3,7 +3,7 @@ const mongoose = require("mongoose");
 const userSchema = new mongoose.Schema({
   username: {type:String, required:true, unique:true},
   email: { type: String,required:true},
-  password: { type: String },
+  password: { type: String, minlength: 8, required: true, },
   createdAt: { type: Date, default: Date.now },
   updatedAt: { type: Date, default: Date.now },
   isAdmin: { type: Boolean, default: false },
