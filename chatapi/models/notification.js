e5ebc2ef5b0371889ff8211f6a7e6f66087d91b7
Nischal Khatri <nischal06khatri@gmail.com>
2022-01-23 12:42:16 +0545

    Creating notification schema

--- chatapi/models/notification.js
+++ chatapi/models/notification.js
@@ -1,7 +1,16 @@
 const mongoose = require("mongoose");
 
 const notificationSchema = new mongoose.Schema({
-    
+    notificationType: { type: String, required: true },
+    notificationTitle: { type: String, required: true },
+    notificationMessage: { type: String, required: true },
+    notificationSenderId: { type: String, required: true },
+    notificationSenderName: { type: String, required: true },
+    notificationSenderAvatar: { type: String, required: true },
+    notificationSenderType: { type: String, required: true },
+    notificationCreatedAt: { type: Date, default: Date.now },
+    notificationUpdatedAt: { type: Date, default: Date.now },
+
 })
 
 module.exports = mongoose.model("notification", notificationSchema);
