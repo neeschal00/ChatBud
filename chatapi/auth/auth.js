09a8fa55587b2be683ce9ed0d50ebe2f2bf2b427
Nischal Khatri <nischal06khatri@gmail.com>
2022-01-31 23:44:02 +0545

    Adding admin authorization check

--- chatapi/auth/auth.js
+++ chatapi/auth/auth.js
@@ -1,24 +1,52 @@
 const jwt = require("jsonwebtoken");
+const userModel = require("../models/userModel");
 
-module.exports.verifyUser = function (req, res, next) {
-    const tokens = req.headers.authorization.split(" ")[1];
-    const data = jwt.verify(tokens, "anysecretkey")
-    console.log(data)
+module.exports.verifyUser = (req, res, next) => {
+    // const tokens = await req.headers.authorization.split(" ")[1];
+    // console.log(tokens);
+    // const data = jwt.verify(tokens, "anysecretkey")
+    // console.log(data)
     try {
-        const token = req.headers.authorization.split(" ")[1];
+        const token =  req.headers.authorization.split(" ")[1];
+        console.log(token);
         const data = jwt.verify(token, "anysecretkey");
         console.log(data.userId);
-        user.findOne({ _id: data.userId })
-            .then(function (result) {
-                console.log(result);
-                req.userInfo = result;
-                next();
-            })
-            .catch(function (e) {
-                res.json({ error: "Invalid Access" })
-            })
+        console.log("user auth hit")
+        const userData = userModel.findOne({ _id: data.userId })
+        // console.log(userData);
+        try{
+            req.userInfo = userData;
+            next();
+        }
+        catch (e) {
+            res.status(403).json({ error: e })
+        }
+            
     } catch (e) {
-        res.json({ error: " Invalid Access" })
+        res.status(400).json({ error: e })
     }
 
+}
+
+exports.adminAuth = (req, res, next) => {
+  const token = req.headers.authorization.split(" ")[1];
+  const data = jwt.verify(token, "anysecretkey");
+  if (token) {
+    jwt.verify(token, "anysecretkey",(err, decodedToken) => {
+      if (err) {
+        return res.status(401).json({ message: "Not authorized" })
+      } else {
+          const userData = user.findOne({ _id: decodedToken.userId })
+        if (!userData.isAdmin) {
+          return res.status(401).json({ message: "Not authorized" })
+        } else {
+          next()
+        }
+      }
+    })
+  } else {
+    return res
+      .status(401)
+      .json({ message: "Not authorized, token not available" })
+  }
 }
