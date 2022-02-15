6cba85d554a690feb9b172c3d0623995de8e3dc4
Nischal Khatri <nischal06khatri@gmail.com>
2022-02-15 17:09:30 +0545

    Adding channel create as well

--- chatapi/routes/chat.js
+++ chatapi/routes/chat.js
@@ -66,7 +66,8 @@ router.post('/create/private',auth.verifyUser,async(req,res)=>{
         const chat = new chatModel({
             chatName: "_",
             chatType: "private",
-            createdBy: req.userInfo._id
+            createdBy: req.userInfo._id,
+            chatIsPrivate: true,
         })
         const chatCreated = await chat.save().catch(e => {res.status(400).json({message: e})});
         const user = await userModel.findById(req.userInfo._id);
@@ -103,16 +104,19 @@ router.post('/create/group',auth.verifyUser,async(req,res)=>{
         const chat = new chatModel({
             chatName: "_",
             chatType: "group",
-            createdBy: req.userInfo._id
+            createdBy: req.userInfo._id,
+            chatIsGroup: true,
         })
         const chatCreated = await chat.save().catch(e => {res.status(400).json({message: e})});
         const user = await userModel.findById(req.userInfo._id);
         user.chats.push(chatCreated._id);
+        user.groups.push(chatCreated._id);
         await user.save();
         chatCreated.chatMembers.push(userData._id);
         for(let i=0;i<data.buddies.length;i++){
             const buddy = await userModel.findById(data.buddies[i]);
             buddy.chats.push(chatCreated._id);
+            buddy.groups.push(chatCreated._id);
             await buddy.save();
             chatCreated.chatMembers.push(data.buddy);
         }
@@ -125,5 +129,65 @@ router.post('/create/group',auth.verifyUser,async(req,res)=>{
 
 })
 
+router.post('/create/channel',auth.verifyUser, (req, res, next) => {
+  const data = req.body;
+  const userData = req.userInfo;
+  const chatExists = await chatModel.findOne({ chatName: data.channelName,createdBy:userData._id,chatType:"channel" });
+  console.log(chatExists);
+  if (chatExists) {
+    res.status(400).json({ message: 'Channel name already exists', success: false });
+    return;
+  }
+  try {
+    const chat = new chatModel({
+      chatName: data.channelName,
+      chatType: "channel",
+      createdBy: req.userInfo._id,
+      chatIsChannel: true,
+    })
+    const chatCreated = await chat.save().catch(e => {res.status(400).json({message: e})});
+    const user = await userModel.findById(req.userInfo._id);
+    user.chats.push(chatCreated._id);
+    await user.save();
+    res.status(200).json({message: "Chat Created"});
+
+  } catch (e) {
+    res.json({ message: e });
+  }
+});
+
+
+router.post('/add/member',auth.verifyUser,async(req,res)=>{
+    const data = req.body;
+    const userData = req.userInfo;
+    const chat = await chatModel.findById(data.chatId);
+    if(!chat){
+        res.status(400).json({message:"Chat Not Found"});
+        return;
+    }
+    if(chat.createdBy.toString()!==userData._id.toString()){
+        res.status(400).json({message:"You are not the owner of this chat"});
+        return;
+    }
+    if(chat.chatType==="private" || chat.chatIsPrivate){
+        const chatExists = await chatModel.findOne({ chatName: "_",createdBy:userData._id,chatType:"private",chatMembers: {"$in":[data.buddy]} });
+        console.log(chatExists);
+        if (chatExists){
+            res.status(400).json({message: "Buddy Already is in the chat"});
+            return;
+        }
+        try{
+            const buddy = await userModel.findById(data.buddy);
+            buddy.chats.push(chat._id);
+            buddy.groups.push(chat._id);
+            await buddy.save();
+            chat.chatMembers.push(data.buddy);
+            await chat.save();
+            res.status(200).json({message: "Buddy Added"});
+        }
+        catch(e){
+            res.status(400).json({message:e});
+        }
+}});
 
 module.exports = router;
